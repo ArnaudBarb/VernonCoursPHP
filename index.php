b9e854cdc995e8d3fe7f891677992a3f69bf9421
Arnaud Barberon <barberon_a@hotmail.fr>
2022-01-25 15:00:51 +0100

    Structures de controle

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+// $valeur = 42;
+
+// if ($valeur > 0){
+//     echo "positif";
+
+// } elseif ($valeur == 0){
+//     echo "valeur nulle";
+
+// } else{
+//     echo "négatif";}
+
+//Correction
+
+$valeur = 42;
+$resultat="";
+
+if(is_numeric($valeur)){
+if($valeur > 0){
+    $resultat = "positive";
+} elseif ($valeur < 0){
+    $resultat ="Négative";
+} else{
+    $resultat = "nulle";
+}
+}
+else $resultat = "non numérique";
+
+echo "La valeur est $resultat.";
+
